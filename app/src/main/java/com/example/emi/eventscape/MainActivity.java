e0f4149fec394268872ffeb4719a9b4b67ea0f79
EmiPali <56865035+EmiPali@users.noreply.github.com>
2019-11-13 21:45:30 +0100

    test

--- app/src/main/java/com/example/emi/eventscape/MainActivity.java
+++ app/src/main/java/com/example/emi/eventscape/MainActivity.java
@@ -1,8 +1,16 @@
 package com.example.emi.eventscape;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +18,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference(); // firebase database reference
+        // add some text
+        reference.setValue("This is a test.")
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Toast.makeText(MainActivity.this, "Success", Toast.LENGTH_SHORT).show();//
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(MainActivity.this, "Failure", Toast.LENGTH_SHORT).show();
+                    }
+                });
     }
 }
